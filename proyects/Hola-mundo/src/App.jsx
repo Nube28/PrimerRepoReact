f1825d98b1616e632f4a044e61bc42ed507604ec
Nube28 <159978169+Nube28@users.noreply.github.com>
2025-02-26 14:54:58 -0700

    app done

--- proyects/Hola-mundo/src/App.jsx
+++ proyects/Hola-mundo/src/App.jsx
@@ -3,22 +3,49 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App () {
 
-  
+  const users = [
+    {
+      id: 1,
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      id: 2,
+      userName: 'pheralb',
+      name: 'Pablo H.',
+      isFollowing: false
+    },
+    {
+      id: 3,
+      userName: 'PacoHdezs',
+      name: 'Paco Hdez',
+      isFollowing: true
+    },
+    {
+      id: 4,
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false
+    }
+  ]
 
   return (
         <section className='App'>
-          <TwitterFollowCard userName="midudev" >
-            Nubecita
-          </TwitterFollowCard>
-
-          <TwitterFollowCard userName="otro" >
-            Papu
-          </TwitterFollowCard>
-
-          <TwitterFollowCard >
-            este no tiene un userName
-          </TwitterFollowCard>
-
+          {
+            users.map(user => {
+              const{ id, userName, name, isFollowing } = user
+              return(
+                <TwitterFollowCard
+                  key={id}
+                  userName={userName}
+                  initialIsFollowing={isFollowing}
+                >
+                  {name}
+                </TwitterFollowCard>
+              )
+            })
+          }
         </section>  
   )
 }
--- proyects/Hola-mundo/src/TwitterFollowCard.jsx
+++ proyects/Hola-mundo/src/TwitterFollowCard.jsx
@@ -1,30 +1,40 @@
-import {useState} from 'react'
+import {useState} from 'react' //esto habilita el usar estasdos
 
-export function TwitterFollowCard({children, userName = 'valor por defecto', name, isFollowing}) {
-    const imageSrc = `https://unavatar.io/${userName}`
+export function TwitterFollowCard({children, userName = 'valor por defecto', initialIsFollowing}) { // al igual un valor ese sera su valor por defecto
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
     
-    const text = isFollowing ? 'Siguiendo' : 'Seguir'
-    const buttonClassName = isFollowing 
+    const imageSrc = `https://unavatar.io/${userName}` //una simple funcion para sacar la imagencita en base al nombre
+    
+    const textButton = isFollowing ? 'Siguiendo' : 'Seguir' //ternario que me dice que texto debe mostrar el botton de seguir
+    const buttonClassName = isFollowing  //si lo sigues muestra este css y si no este otro
         ? 'tw-followCard-button is-following'
         : 'tw-followCard-button'
+
+    const handleClick = () => { //arrow que cambia el estado del botton 
+        setIsFollowing(!isFollowing)
+    }
+
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img 
-                    src={imageSrc} 
+                    src={imageSrc} //llamamos la const de arriba
                     alt="El avatar de Usuario" 
                     className='tw-followCard-avatar'/>
                 <div className='tw-followCard-info'>
-                    <strong>{children}</strong>
+                    <strong>
+                        {children} 
+                    </strong> 
                     <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
 
             <aside>
-                <button className={buttonClassName}>
-                    {text}
+                <button className={buttonClassName} onClick={handleClick}>{/*la claseName depende de la funcion de arriba y cuando se clicke se llama a la  otra*/}
+                    <span className='tw-followCard-text'>{textButton}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de segir</span>
                 </button>
             </aside>
         </article>
     )
-}
+} 
